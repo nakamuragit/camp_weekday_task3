0c1809d459f8aac7b3b6b63e4b37f6acf13761be
Nakamura <gireru@hotmail.co.jp>
2019-03-15 14:16:03 +0900

    レビューの修正

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -1,25 +1,25 @@
 module Common 
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
     
   def calculation
-    @second = params[:second] 
-    @first = params[:first]
-    @operator = params[:operator]
-    if @operator == "addition"
-        @output= @first.to_i + @second.to_i
-    elsif @operator == "subtraction"
-        @output = @first.to_i - @second.to_i
-    elsif @operator == "multiplication"
-        @output = @first.to_i * @second.to_i
-    elsif @operator == "division"
-        begin
-            answer = @first.to_i / @second.to_i
-            remainder = @first.to_i % @second.to_i
-            @output = answer.. remainder 
-        rescue ZeroDivisionError => error
-            @output = error
-        end
-    @output
+    second = params[:second].to_i 
+    first = params[:first].to_i
+    operator = params[:operator]
+    if operator == "addition"
+      output= first.to_i + second.to_i
+    elsif operator == "subtraction"
+      output = first.to_i - second.to_i
+    elsif operator == "multiplication"
+      output = first.to_i * second.to_i
+    elsif operator == "division"
+      begin
+        answer = first.to_i / second.to_i
+        remainder = first.to_i % second.to_i
+        output = answer.. remainder 
+      rescue ZeroDivisionError => error
+        output = error
+      end
+    output
     end
   end
 end
